d37591f6c4beae10caaf6f131f22aebc4ec91d17
Olga Melnikova <holywindmill@gmail.com>
2019-09-13 09:57:10 +0400

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,14 @@
+//1.
+let algebra = 5, geography = 4, physics = 4;
+let averageScore = (algebra + geography + physics) / 3;
+console.log(averageScore);
+
+//2.
+let name = 'Ольга';
+let message = (`Привет, мир! Меня зовут ${name}`);
+console.log(message);
+
+//3.
+let x = 2, y = 22, z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
