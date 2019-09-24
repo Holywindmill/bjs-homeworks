955091e1a2a09f7c8901e7cc39e59a408a6fc456
Olga Melnikova <holywindmill@gmail.com>
2019-09-24 22:55:55 +0400

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -10,10 +10,29 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    let months = monthDiff(new Date(), new Date(date));
+    let result = 0;
+    let monthlyRate = percent / 100 / 12;
+    let amountToPay = amount - contribution;
+    let payment = amountToPay * (monthlyRate + monthlyRate / ((Math.pow(1 + monthlyRate, months)) - 1));
+    return (payment * months).toFixed(2);
 }
+
+function monthDiff(dateFrom, dateTo) {
+    return dateTo.getMonth() - dateFrom.getMonth() +
+        (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
+}
+
+
+console.log(calculateTotalMortgage(10, 0, 50000, new Date(2020,9,24)));
+console.log(calculateTotalMortgage(10, 1000, 50000, new Date(2020,9,24)));
+console.log(calculateTotalMortgage(10, 0, 20000, new Date(2021,9,24)));
+console.log(calculateTotalMortgage(10, 1000, 20000, new Date(2021,9,24)));
+console.log(calculateTotalMortgage(10, 20000, 20000, new Date(2021,9,24)));
+console.log(calculateTotalMortgage(10, 0, 10000, new Date(2022,9,24)));
+console.log(calculateTotalMortgage(15, 0, 10000, new Date(2022,9,24)));
+
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -23,6 +42,15 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (name === null || name === undefined || name === '') {
+        name = 'Аноним'
+    }
+    return `Привет, мир! Меня зовут ${name}.`;
 }
+
+console.log(getGreeting('Иван'));
+console.log(getGreeting('Анастасия'));
+console.log(getGreeting('Петр'));
+console.log(getGreeting(null));
+console.log(getGreeting(undefined));
+console.log(getGreeting(''));
